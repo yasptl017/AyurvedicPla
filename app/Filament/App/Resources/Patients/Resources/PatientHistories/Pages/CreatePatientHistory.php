f1981874c73ba49193be1b8305a6dd06fae16f94
Yagnesh Patel <yagneshrpatel1993@gmail.com>
2026-03-11 18:56:00 +0530

    Save button on patient history page..

--- app/Filament/App/Resources/Patients/Resources/PatientHistories/Pages/CreatePatientHistory.php
+++ app/Filament/App/Resources/Patients/Resources/PatientHistories/Pages/CreatePatientHistory.php
@@ -4,6 +4,7 @@ namespace App\Filament\App\Resources\Patients\Resources\PatientHistories\Pages;
 
 use App\Filament\App\Resources\Patients\PatientResource;
 use App\Filament\App\Resources\Patients\Resources\PatientHistories\PatientHistoryResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePatientHistory extends CreateRecord
@@ -12,19 +13,54 @@ class CreatePatientHistory extends CreateRecord
 
     protected function getRedirectUrl(): string
     {
-        $patientId = $this->getRecord();
+        $record = $this->getRecord();
 
         return PatientResource::getUrl('edit', [
-            'record' => $patientId->PatientId,
+            'record' => $record->PatientId,
             'relation' => '0',
         ]);
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('print')
+                ->button()
+                ->color('gray')
+                ->icon('heroicon-o-printer')
+                ->disabled(),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('save')
+                ->label('Save')
+                ->color('primary')
+                ->action(function () {
+                    $this->create(another: false);
+                    if ($this->getRecord()) {
+                        $this->redirect(
+                            PatientHistoryResource::getUrl('edit', [
+                                'record' => $this->getRecord(),
+                                'patient' => $this->getRecord()->PatientId,
+                            ])
+                        );
+                    }
+                }),
+
+            $this->getCreateFormAction()
+                ->label('Create'),
+
+            $this->getCancelFormAction(),
+        ];
     }
 
     public function getBreadcrumbs(): array
     {
         $breadcrumbs = parent::getBreadcrumbs();
 
-        // Find and update the Patient edit page breadcrumb to use relation=0
         foreach ($breadcrumbs as $url => $label) {
             if (str_contains($url, 'relation=patientHistories')) {
                 $newUrl = str_replace('relation=patientHistories', 'relation=0', $url);
