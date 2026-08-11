b404be4005fd8de48bef81936e7db0092869d70c
Yagnesh Patel <yagneshrpatel1993@gmail.com>
2026-08-11 11:56:45 +0530

    phtml

---
+++ public_html/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Laravel launcher for Hostinger public_html deployment.
+ *
+ * Requests are routed into the sibling `public` folder.
+ */
+
+require __DIR__ . '/../public/index.php';
